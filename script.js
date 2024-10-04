e9e010545b94b8ec8a89ec0eb0be595e38ea3bd8
Sjoerd473 <sjoerdbrussee@hotmail.com>
2024-10-04 09:44:44 +0200

    added printBoard, to log the gameboard to the console, checkBoard, to put a token on the board, and setToken, to let the cell change its token value

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
+const players = [{
+    name: 'playerOneName',
+    token: 'X'
+}, {
+    name: 'playerTwoName',
+    token: 'O'
+}];
+
+const row = 3;
+const column = 3;
+let board = [];
+
+const createBoard = () => {
+    for (let i = 0; i < row; i++) {
+        board[i] = []
+        for (let j = 0; j < column; j++) {
+            board[i].push(Cell())
+        }
+    }
+}
+
+const getBoard = () => board;
+
+const printBoard = () => {
+        //call getValue on all tiles, put them into an array, and console.table the result
+        let logBoard = [];
+        for (let i = 0; i < row; i++) {
+            logBoard[i] = []
+            for (let j = 0; j < column; j++) {
+                logBoard[i].push(board[i][j].getValue())
+            }
+            
+        } console.table(logBoard)
+    }
+
+const checkBoard = (row, column, activePlayer) => {
+    if (board[row][column].getValue() === 0) {
+        board[row][column].setToken(activePlayer.token)
+    } else {
+        console.log('Square is occupied, please select a different square.')
+    }
+}
+
+
 //createBoard creates a 2d array, 9x9, and assigns Cell to each square, that will let us manipulate the array to play tic tac toe.
 
-function Gameboard(){
+function Gameboard() {
     const row = 3;
     const column = 3;
     let board = [];
 
     const createBoard = () => {
-        for (let i = 0; i < row; i++){
+        for (let i = 0; i < row; i++) {
             board[i] = []
-            for(let j = 0; j < column; j++){
+            for (let j = 0; j < column; j++) {
                 board[i].push(Cell())
             }
         }
@@ -17,33 +61,44 @@ function Gameboard(){
     const getBoard = () => board;
 
     const printBoard = () => {
-        console.log(board)
+        //call getValue on all tiles, put them into an array, and console.table the result
+        for (let i = 0; i < row; i++) {
+            logBoard[i] = []
+            for (let j = 0; j < column; j++) {
+                logBoard[i].push(board[i][j].getValue())
+            }
+            console.table(logBoard)
+        }
     }
 
-    const checkBoard = () => {
-        console.log('walk the board')
+    const checkBoard = (row, column, activePlayer) => {
+        if (!board[row][column].getValue() === 0) {
+            board[row][column].setToken(activePlayer.token)
+        } else {
+            console.log('Square is occupied, please select a different square.')
+        }
     }
 
-    return {getBoard, printBoard, checkBoard}
+    return { getBoard, printBoard, checkBoard }
 }
 
-function Cell(){
+function Cell() {
     let value = 0;
 
-    const getValue = () =>  value;
+    const getValue = () => value;
 
-    const setToken = () => {
-        console.log('boop')
+    const setToken = (playerToken) => {
+        value = playerToken
     }
-    return {getValue, setToken}
+    return { getValue, setToken }
 }
 
-function Gamestate(playerOneName = 'playerOne', playerTwoName = 'playerTwo'){
+function Gamestate(playerOneName = 'playerOne', playerTwoName = 'playerTwo') {
 
     const players = [{
-        name : playerOneName,
+        name: playerOneName,
         token: 'X'
-    },{
+    }, {
         name: playerTwoName,
         token: 'O'
     }];
@@ -52,12 +107,16 @@ function Gamestate(playerOneName = 'playerOne', playerTwoName = 'playerTwo'){
 
     const board = Gameboard()
 
-    const switchActivePlayer = () => {};
+    const switchActivePlayer = () => { };
 
-    const printNewRound = () => {};
+    const printNewRound = () => { };
 
-    const getActivePlayer = () => {};
+    const getActivePlayer = () => { };
 
-    const playRound = (row,column) => {};
-    return {playRound}
+    const playRound = (row, column) => { };
+
+    const checkWin = () => {
+        //call getValue on tiles to check for winning layouts
+    };
+    return { playRound }
 }
